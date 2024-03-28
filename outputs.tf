79eaf0665cf8af0789bfd36ff893857990b30647
Jordan Acosta <jordanjacosta@gmail.com>
2024-03-27 20:13:19 -0700

    docs: update variable and output descriptions

--- outputs.tf
+++ outputs.tf
@@ -4,14 +4,16 @@ output "runner" {
     odr_iam_role_arn     = module.odr_iam_role.iam_role_arn
     install_iam_role_arn = module.install_iam_role.iam_role_arn
   }
+  description = "A map of runner attributes: runner_iam_role_arn, odr_iam_role_arn, install_iam_role_arn"
 }
 
 output "ecs_cluster" {
   value = {
     arn  = module.ecs_cluster.cluster_arn,
     id   = module.ecs_cluster.cluster_id,
-    name = local.cluster_name,
+    name = module.ecs_cluster.name,
   }
+  description = "A map of ecs_cluster attributes: arn, id, name."
 }
 
 output "vpc" {
@@ -32,6 +34,7 @@ output "vpc" {
     default_security_group_id  = aws_security_group.runner.id
     default_security_group_arn = aws_security_group.runner.arn
   }
+  description = "A map of vpc attributes: name, id, cidr, azs, private_subnet_cidr_blocks, private_subnet_ids, public_subnet_cidr_blocks, public_subnet_ids, default_security_group_id, default_security_group_arn."
 }
 
 output "account" {
@@ -39,6 +42,7 @@ output "account" {
     id     = data.aws_caller_identity.current.account_id
     region = var.region
   }
+  description = "A map of AWS account attributes: id, region"
 }
 
 output "ecr" {
@@ -49,6 +53,7 @@ output "ecr" {
     registry_id     = module.ecr.repository_registry_id
     registry_url    = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.region}.amazonaws.com"
   }
+  description = "A map of ECR attributes: repository_url, repository_arn, repository_name, registry_id, registry_url."
 }
 
 output "public_domain" {
@@ -57,6 +62,7 @@ output "public_domain" {
     name        = aws_route53_zone.public.name
     zone_id     = aws_route53_zone.public.id
   }
+  description = "A map of public Route53 zone attributes: nameservers, name, zone_id."
 }
 
 output "internal_domain" {
@@ -65,4 +71,5 @@ output "internal_domain" {
     name        = aws_route53_zone.internal.name
     zone_id     = aws_route53_zone.internal.id
   }
+  description = "A map of internal Route53 zone attributes: nameservers, name, zone_id."
 }
