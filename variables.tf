73cc6ebc676a5158e2e5beacafb1763334dee831
Jordan Acosta <jordan@nuon.co>
2024-03-29 07:28:57 -0700

    docs: update variable and output descriptions (#6)

--- variables.tf
+++ variables.tf
@@ -4,64 +4,53 @@ locals {
   tags           = merge({ nuon_id = var.nuon_id }, var.tags)
   nuon_id        = var.nuon_id
   cluster_name   = (var.prefix_override != "" ? var.prefix_override : var.nuon_id)
-
-  /* external_dns = { */
-  /*   registry           = "txt" */
-  /*   provider           = "aws" */
-  /*   policy             = "sync" */
-  /*   triggerLoopOnEvent = true */
-  /*   interval           = "15m" */
-  /* } */
 }
 
 variable "prefix_override" {
   type        = string
-  description = "The resource prefix to override, otherwise defaults to the nuon install id"
+  description = "Prefix used to name sandbox resources. Will default to the Nuon ID."
   default     = ""
 }
 
 variable "vpc_id" {
   type        = string
-  description = "The ID of VPC to deploy the ECS cluster to"
+  description = "The ID of the AWS VPC to provision the sandbox in."
 }
+
+# These will be set by Nuon during the install provision process.
 
 variable "nuon_id" {
   type        = string
-  description = "The nuon id for this install. Used for naming purposes."
+  description = "An ID used to name resources. Defaults to the install ID. Will be set by Nuon during the install provision process."
 }
 
 variable "assume_role_arn" {
   type        = string
-  description = "The role arn to assume during provisioning of this sandbox."
+  description = "The ARN of the AWS IAM Role to assume during provisioning of the sandbox. Will be set by Nuon during the install provision process."
 }
 
 variable "tags" {
   type        = map(any)
-  description = "List of custom tags to add to the install resources. Used for taxonomic purposes."
+  description = "List of tags to add to all sandbox resources (that support tags). Used for taxonomic purposes. Will be set by Nuon during the install provision process."
 }
 
 variable "region" {
   type        = string
-  description = "The region to launch the cluster in"
-
-  validation {
-    condition     = contains(["us-east-1", "us-east-2", "us-west-1", "us-west-2", ], var.region)
-    error_message = "${var.region} is currently unsupported"
-  }
+  description = "The AWS region to provision the sandbox in. Will be set by Nuon during the install provision process."
 }
 
 // NOTE: if you would like to create an internal load balancer, with TLS, you will have to use the public domain.
 variable "internal_root_domain" {
   type        = string
-  description = "internal root domain."
+  description = "The internal root domain of the sandbox. Will be set by Nuon during the install provision process."
 }
 
 variable "public_root_domain" {
   type        = string
-  description = "public root domain."
+  description = "The public root domain of the sandbox. Will be set by Nuon during the install provision process."
 }
 
 variable "nuon_runner_install_trust_iam_role_arn" {
   type        = string
-  description = "IAM role to grant Nuon access to install the runner."
+  description = "The ARN of the AWS IAM Role to grant Nuon access to install the ECS Fargate runner. Will be set by Nuon during the install provision process."
 }
